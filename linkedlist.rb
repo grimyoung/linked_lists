ab4f3bed02a433f62e2310e46d4e046150a04f3a
grimyoung <samhyukim@gmail.com>
2017-10-03 20:46:20 -0700

    added LinkedList class with #append, #prepend, and #size methods

---
+++ linkedlist.rb
@@ -0,0 +1,36 @@
+class LinkedList
+  def initialize(value)
+    @head = Node.new(value,nil)
+  end
+
+  def append(value)
+    if @head == nil
+      prepend(value)
+    end
+    else 
+      temp = @head
+      while temp.next_node != nil
+        temp = temp.next_node
+      end
+      @temp.next_node = Node.new(value,nil)
+    end
+  end
+
+  def prepend(value)
+    @head = Node.new(value,@head)
+  end
+
+  def size
+    count = 0
+    if @head != nil
+      temp = @head
+      while temp.next_node != nil
+        count += 1
+        temp.next_node
+      end
+    end
+    return count
+  end
+
+  
+end

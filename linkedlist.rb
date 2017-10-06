31e0f952cece7c8542e8a22846eee1db480814d6
grimyoung <samhyukim@gmail.com>
2017-10-05 21:09:20 -0700

    added #head, #tail, #at(index)

--- linkedlist.rb
+++ linkedlist.rb
@@ -6,7 +6,6 @@ class LinkedList
   def append(value)
     if @head == nil
       prepend(value)
-    end
     else 
       temp = @head
       while temp.next_node != nil
@@ -24,14 +23,44 @@ class LinkedList
     count = 0
     if @head != nil
       temp = @head
-      count += 1
+      count = 1
       while temp.next_node != nil
         count += 1
-        temp.next_node
+        temp = temp.next_node
       end
     end
     return count
   end
 
 
+  def head
+    return @head
+  end
+
+  def tail
+    temp = @head
+    while temp.next_node != nil
+      temp = temp.next_node
+    end
+  end
+
+  def at(index)
+    temp = @head
+    index.times do 
+      if temp.next_node == nil
+        return nil
+      else
+        temp = temp.next_node
+      end
+    end
+    return temp
+  end
+
+  def to_s
+    temp = @head
+    while temp.next_node != nil
+      print "(#{temp.value}) -> "
+      temp = temp.next_node
+    end
+  end
 end
